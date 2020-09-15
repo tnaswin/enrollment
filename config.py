a1bb50e3131d6c90d9882a60d77974007894b39e
tnaswin <tnaswin007@gmail.com>
2020-09-15 23:17:24 +0530

    authentication and creating sessions

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\t\x93\x19 \xb7\xb9\xb9x/N{\x12\xf3\xf2\x8b\xa6'
 
     MONGODB_SETTINGS = { 'db' : 'Tech_Enroll'}
